0fcfd0d1dab84ed9c38ccf5a8d3106a2544728b1
es-souabni <karimessouabni0@gmail.com>
2024-06-18 14:07:08 +0200

    Update inject_secret_to_vault.py

--- inject_secret_to_vault.py
+++ inject_secret_to_vault.py
@@ -1,26 +1,43 @@
-import yaml
+import json
 import hvac
 
-def load_secrets_from_yaml(file_path):
-    with open(file_path, 'r') as file:
-        data = yaml.safe_load(file)
-        return data['secrets']
+def load_secrets_from_file(file_path):
+    """Load secrets from a JSON file."""
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    return data['data']
 
-def inject_secrets_to_vault(secrets, vault_url, vault_token, secrets_path):
+def authenticate_to_vault(client, username, password):
+    """Authenticate to Vault using username and password."""
+    response = client.auth.userpass.login(
+        username=username,
+        password=password
+    )
+    client.token = response['auth']['client_token']
+
+def inject_secrets_to_vault(secrets, vault_url, vault_token, secret_path):
+    """Inject secrets into Vault."""
     client = hvac.Client(url=vault_url, token=vault_token)
-    for path, secrets_data in secrets.items():
-        full_path = f'{secrets_path}/{path}'
-        client.secrets.kv.v2.create_or_update_secret(path=full_path, secret=secrets_data)
-        print(f"Injected secrets into {full_path}")
+    if client.is_authenticated():
+        response = client.secrets.kv.v2.create_or_update_secret(
+            path=secret_path,
+            secret=secrets
+        )
+        print("Secrets injected:", response)
+    else:
+        print("Failed to authenticate with Vault.")
 
 def main():
-    yaml_file_path = 'secrets.yaml'
     vault_url = 'http://localhost:8200'
-    vault_token = 'your_vault_token_here'
-    secrets_path = 'mysecrets'  # This is the base path where secrets will be stored in Vault
+    username = 'your-username'  # Replace with your Vault username
+    password = 'your-password'  # Replace with your Vault password
+    secret_path = 'secret/data/myapp'  # Adjust the path according to your Vault setup
 
-    secrets = load_secrets_from_yaml(yaml_file_path)
-    inject_secrets_to_vault(secrets, vault_url, vault_token, secrets_path)
+    client = hvac.Client(url=vault_url)
+    authenticate_to_vault(client, username, password)
+
+    secrets = load_secrets_from_file('secrets.json')
+    inject_secrets_to_vault(secrets, vault_url, client.token, secret_path)
 
 if __name__ == '__main__':
     main()
