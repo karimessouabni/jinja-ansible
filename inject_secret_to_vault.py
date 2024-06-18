5edaf9dfc7fd5da91ba75ee7edc00d2bc76b17c7
es-souabni <karimessouabni0@gmail.com>
2024-06-18 14:18:28 +0200

    Update inject_secret_to_vault.py

--- inject_secret_to_vault.py
+++ inject_secret_to_vault.py
@@ -7,16 +7,9 @@ def load_secrets_from_yaml(file_path):
         data = yaml.safe_load(file)
     return data['data']
 
-def authenticate_to_vault(client, username, password):
-    """Authenticate to Vault using username and password."""
-    response = client.auth.userpass.login(
-        username=username,
-        password=password
-    )
-    client.token = response['auth']['client_token']
-
-def inject_secrets_to_vault(client, secrets, secret_path):
+def inject_secrets_to_vault(vault_url, vault_token, secrets, secret_path):
     """Inject secrets into Vault."""
+    client = hvac.Client(url=vault_url, token=vault_token)
     if client.is_authenticated():
         response = client.secrets.kv.v2.create_or_update_secret(
             path=secret_path,
@@ -27,16 +20,12 @@ def inject_secrets_to_vault(client, secrets, secret_path):
         print("Failed to authenticate with Vault.")
 
 def main():
-    vault_url = 'http://localhost:8200'
-    username = 'your-username'  # Replace with your Vault username
-    password = 'your-password'  # Replace with your Vault password
+    vault_url = 'http://localhost:8200'  # Adjust if your Vault server is not running locally
+    vault_token = 'your-root-token'  # Replace with your initial Vault root token
     secret_path = 'secret/data/myapp'  # Adjust the path according to your Vault setup
 
-    client = hvac.Client(url=vault_url)
-    authenticate_to_vault(client, username, password)
-
     secrets = load_secrets_from_yaml('secrets.yaml')
-    inject_secrets_to_vault(client, secrets, secret_path)
+    inject_secrets_to_vault(vault_url, vault_token, secrets, secret_path)
 
 if __name__ == '__main__':
     main()
